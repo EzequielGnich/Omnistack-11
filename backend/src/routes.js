82cde08dc3c4325dffe316c94ef104deb30adb8b
Ezequiel Gnich <ezequielgnich@live.com>
2020-03-24 19:25:45 -0300

    criada a rota /ongs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const crypto = require('crypto');
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-    return res.json({evento: "semana"});
+const connection = require('./database/connection');
+
+routes.get('/ongs', async (req, res) => {
+    const ongs = await connection('ongs').select('*');
+
+    if (!ongs[0]) return res.json({ msg: "Não existem ongs cadastradas no banco de dados"}) 
+
+    return res.json({ongs});
+});
+
+routes.post('/ongs', async (req, res) => {
+    const { name, email, whatsapp, city, uf } = req.body;
+
+    const id = crypto.randomBytes(4).toString('HEX');
+
+    await connection('ongs').insert({
+        id, name, email, whatsapp, city, uf
+    });
+
+    return res.json({ id });
 })
 
 module.exports = routes;
